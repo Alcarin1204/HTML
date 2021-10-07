96095b60933d530d8ff1bbd32c1b1c6120f96603
Alcarin1204 <70557802+Alcarin1204@users.noreply.github.com>
2021-10-07 23:07:44 +0700

    Add files via upload

---
+++ HTML-main/index.js
@@ -0,0 +1,78 @@
+function isAdd() {//добавление сьроки в список
+    const str = document.getElementById("box").value;
+    if (str === "") {
+        return;
+    }
+    array.push({title: str, isChecked: false});
+    Filter();
+    document.getElementById("box").value = "";//очистка поля после добавления элемента
+}
+
+const array = []
+let currentFilter = "";
+
+function render(array){
+             
+    const getList = document.getElementById("list-items");
+    getList.innerHTML="";
+    array.forEach((e,index) =>{
+        const text = document.createElement("span");
+        text.innerText = e.title; 
+        const element = document.createElement("li");
+
+        const but = document.createElement("button");
+        but.appendChild(document.createTextNode("Удалить"));
+        but.addEventListener("click", ()=>del(e));//навешивание листенера на кнопку удаления
+    
+        const checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.addEventListener("click", ()=>checkBox_Checked(e)); //навешивание листенера на чекбоксы  
+        checkBox.checked = e.isChecked;
+
+        if (checkBox.checked){
+            text.style.textDecoration = "line-through";
+        }
+        
+        element.appendChild(text);
+        element.appendChild(checkBox);
+        element.appendChild(but);
+        getList.appendChild(element);
+        }
+    );
+}
+
+function del(element) {//удаление строки из списка
+    const findDelIndx= array.findIndex(e=>e===element);
+
+    array.splice(findDelIndx, 1);
+    Filter();
+}
+
+function checkBox_Checked(element){//перечеркивание отмеченных строк
+    const findDelIndx= array.findIndex(e=>e===element);
+
+    array[findDelIndx].isChecked=!array[findDelIndx].isChecked;
+    Filter();
+}
+
+function changeFilter(event){
+    event.preventDefault();
+    if(currentFilter===event.target.textContent){
+        return;
+    }
+    currentFilter=event.target.textContent;
+    Filter();
+}
+
+function Filter(){
+    const filterList = [];
+    if(currentFilter==="Сделано"||currentFilter==="Не сделано"){
+        array.forEach(e=>{
+            if(e.isChecked===(currentFilter==="Сделано"))
+            filterList.push(e);
+        });
+        render(filterList);
+        return;
+    }
+    render(array);
+}
